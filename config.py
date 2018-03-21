0f7c5f00873b65b754be267b963a7bb701c986b5
Hoanh An <hoanhan101@gmail.com>
2018-03-21 01:24:31 -0400

    add config file

---
+++ config.py
@@ -0,0 +1,18 @@
+"""
+    config.py - Configuration hub
+    Author: Hoanh An (hoanhan@bennington.edu)
+    Date: 03/21/18
+"""
+
+LOGO = """
+
+    █▀▀█ █▀▀█ █▀▀ █▀▀▄ ░ ░ █▀▀▄ █▀▀ █▀▀
+    █░░█ █░░█ █▀▀ █░░█ ▀ ▀ █░░█ █▀▀ █░░
+    ▀▀▀▀ █▀▀▀ ▀▀▀ ▀░░▀ ░ ░ ▀▀▀░ ▀▀▀ ▀▀▀
+
+"""
+
+
+# Data default paths 
+default_ratings = 'ml-latest-small/ratings.csv'
+default_movies = 'ml-latest-small/movies.csv'
