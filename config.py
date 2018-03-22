9aa59d47d1afc2a8c069f0d312937f3a7dcc0585
Hoanh An <hoanhan101@gmail.com>
2018-03-22 16:35:56 -0400

    add worker constant

--- config.py
+++ config.py
@@ -12,7 +12,16 @@ LOGO = """
 
 """
 
+# Dataset location
+RATINGS_PATH = 'ml-latest-small/ratings.csv'
+MOVIES_PATH = 'ml-latest-small/movies.csv'
 
-# Data default paths 
-default_ratings = 'ml-latest-small/ratings.csv'
-default_movies = 'ml-latest-small/movies.csv'
+# Matrix factorization constants
+MF_STEPS = 3
+MF_GAMMA = 0.001
+MF_LAMDA = 0.02
+
+# Worker
+NUMS_WORKERS = 1
+NUMS_RECOMMENDATIONS = 3
+MAX_ID = 10
