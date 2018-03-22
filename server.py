aff025debf3ac30a1f84d3b808918bd64a2adc5a
Hoanh An <hoanhan101@gmail.com>
2018-03-22 16:38:44 -0400

    use thread instead

--- server.py
+++ server.py
@@ -4,10 +4,13 @@
     Date: 03/22/18
 """
 
+import threading
+
 from flask import Flask, jsonify, render_template
 
 from worker import Worker
 from open_rec import OpenREC
+from config import *
 
 app = Flask(__name__)
 
@@ -28,13 +31,49 @@ def find(number):
     """
     TODO
     """
-    rec = OpenREC()
-    top_recommendations = rec.get_top_recommendations(0, number, 3)
+    threads = []
+
+    worker_TF = Worker(thread_id=0,
+                       task='top_favorites',
+                       user_id=number,
+                       limit=NUMS_RECOMMENDATIONS)
+
+    worker_NN = Worker(thread_id=1,
+                       task='nearest_neighbours',
+                       user_id=number,
+                       limit=NUMS_RECOMMENDATIONS)
+
+    worker_LT = Worker(thread_id=2, 
+                       task='latent_factors',
+                       user_id=number, 
+                       limit=NUMS_RECOMMENDATIONS)
+
+    worker_CC = Worker(thread_id=3,
+                       task='collisions',
+                       user_id=number,
+                       limit=NUMS_RECOMMENDATIONS)
+
+    worker_TF.start()
+    worker_NN.start()
+    worker_LT.start()
+    worker_CC.start()
+
+    threads.append(worker_TF)
+    threads.append(worker_NN)
+    threads.append(worker_LT)
+    threads.append(worker_CC)
+
+    for t in threads:
+        t.join()
 
     response = {
         'header': 'User {}\'s profile'.format(number),
-        'top_recommendations': top_recommendations
+        'top_favorites': worker_TF.data,
+        'nearest_neighbours': worker_NN.data,
+        'latent_factors': worker_LT.data,
+        'collisions': worker_CC.data
     }
+
     return render_template('user.html', data=response)
 
 
