0ff69d7d1cfe7009c3f93a35b55605f07f9ed876
Hoanh An <hoanhan101@gmail.com>
2018-03-22 02:42:27 -0400

    add server

---
+++ server.py
@@ -0,0 +1,42 @@
+"""
+    server.py - OpenREC's API
+    Author: Hoanh An (hoanhan@bennington.edu)
+    Date: 03/22/18
+"""
+
+from flask import Flask, jsonify, render_template
+
+from worker import Worker
+from open_rec import OpenREC
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def welcome():
+    """
+    Test endpoint.
+    """
+    response = {
+        'header': 'Welcome to OpenREC!',
+    }
+    return render_template('landing.html', data=response)
+
+
+@app.route('/user/<int:number>', methods=['GET'])
+def find(number):
+    """
+    TODO
+    """
+    rec = OpenREC()
+    top_recommendations = rec.get_top_recommendations(0, number, 3)
+
+    response = {
+        'header': 'User {}\'s profile'.format(number),
+        'top_recommendations': top_recommendations
+    }
+    return render_template('user.html', data=response)
+
+
+if __name__ == '__main__':
+    app.run()
