107b5bbc2f29de720ebcc64bd855a2eee1f566b3
Hoanh An <hoanhan@bennington.edu>
2018-04-27 15:52:28 -0400

    30 steps 10 recs user_id 1-100

--- config.py
+++ config.py
@@ -18,12 +18,12 @@ MOVIES_PATH = 'ml-latest-small/movies.csv'
 
 # Matrix factorization constants
 MF_DEBUG = True
-MF_STEPS = 20
+MF_STEPS = 30
 MF_GAMMA = 0.001
 MF_LAMDA = 0.02
 
 # Number of recommendation output 
-NUMS_RECOMMENDATIONS = 20
+NUMS_RECOMMENDATIONS = 10
 
 # For target_random and build data only
 NUMS_WORKERS = 1
